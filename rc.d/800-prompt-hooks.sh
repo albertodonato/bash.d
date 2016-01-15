60beaa65dd995690c4f201dee6c8805142cb2767
Alberto Donato <alberto.donato@gmail.com>
2016-01-16 01:11:21 +0200

    Simplify.

--- rc.d/800-prompt-hooks.sh
+++ rc.d/800-prompt-hooks.sh
@@ -5,5 +5,5 @@
 
 set_prompt extended
 set_prompt_info container '2;49;33' "$(container_type)"
-set_prompt_hook bzr '1;49;35' "bzr_pipe_if_different \"\$PWD\""
+set_prompt_hook bzr '1;49;35' "bzr_pipe_if_different"
 set_prompt_hook git '1;49;35' "git rev-parse --abbrev-ref HEAD 2>/dev/null"
