a02b1c8743d111ee78632a69adab87ce157b2b55
Alberto Donato <alberto.donato@gmail.com>
2016-02-16 09:43:57 +0200

    Move bzr-pipes-size to a script.

--- lib/devel-bzr.sh
+++ lib/devel-bzr.sh
@@ -1,24 +1,6 @@
 # -*- mode: sh -*-
 
 #
-# Whether the dir is a bzr lightweight checkout
-#
-# Usage: bzr_is-lw_checkout [path]
-#
-bzr_is_lw_checkout() {
-    _bzr_info "$1" | grep -q "light checkout"
-}
-
-#
-# Return the name of the current pipe in the pipeline
-#
-# Usage: bzr_current_pipe [path]
-#
-bzr_current_pipe() {
-    bzr pipes "$1" | awk '/^\*/ { print $2; }'
-}
-   
-#
 # Return the name of the current pipe if different from the branch name.
 #
 # Usage: bzr_pipe_if_different [path]
@@ -26,29 +8,19 @@ bzr_current_pipe() {
 bzr_pipe_if_different() {
     local path
     [ "$1" ] && path="$1" || path="$PWD"
-    if bzr_is_lw_checkout "$path"; then
+    if _bzr_is_lw_checkout "$path"; then
         local dir=$(basename "$path")
-        local pipe=$(bzr_current_pipe "$path")
+        local pipe=$(_bzr_current_pipe "$path")
         [ "$dir" != "$pipe" ] && echo "$pipe"
     fi
 }
 
-#
-# Return a list of bzr pipes in the pipeline, with their diff size.
-#
-# Usage: bzr_pipes_size
-#
-bzr_pipes_size() {
-    local pipe prev_pipe revspec
+_bzr_is_lw_checkout() {
+    _bzr_info "$1" | grep -q "light checkout"
+}
 
-    for pipe in $(bzr pipes | awk '{print $NF;}'); do
-        [ "$prev_pipe" ] && revspec=":pipe:$prev_pipe" || revspec="ancestor:"
-        revspec+="..:pipe:$pipe"
-
-        local lines=$(bzr diff -r $revspec 2>/dev/null | wc -l)
-        printf "%4d %s\n" $lines $pipe
-        prev_pipe=$pipe
-    done
+_bzr_current_pipe() {
+    bzr pipes "$1" | awk '/^\*/ { print $2; }'
 }
 
 _bzr_info() {
