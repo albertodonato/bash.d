0dd9ca5294def5098c95fa007533bee7213409e9
Alberto Donato <alberto.donato@gmail.com>
2016-05-21 12:37:55 +0300

    Fix node path.

--- rc.d/000-vars.sh
+++ rc.d/000-vars.sh
@@ -6,7 +6,7 @@
 # Keep the original path, so it can be reapplied on reload
 [ -z "$_PATH" ] && _PATH="$PATH"
 PATH="$HOME/bin:$SYSTEM_DIR/bin:$SYSTEM_BASH_DIR/bin:$_PATH"
-PATH+=":$HOME/.local/bin:$HOME/.node/bin"
+PATH+=":$HOME/.local/bin:$HOME/node_modules/.bin"
 
 export HISTFILESIZE=100000
 
