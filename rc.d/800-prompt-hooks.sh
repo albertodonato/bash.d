f44c0523d3c016f704682574cf25988bc484ad5d
Alberto Donato <alberto.donato@gmail.com>
2016-07-16 23:28:48 +0200

    Switch all colors to solarized.

--- rc.d/800-prompt-hooks.sh
+++ rc.d/800-prompt-hooks.sh
@@ -3,7 +3,7 @@
 # Hooks for setting prompt elements
 #
 
-prompt_set_flag container yellow "$(container_type)"
-prompt_set_hook bzr lightmagenta "bzr_pipe_if_different"
-prompt_set_hook git lightmagenta "git rev-parse --abbrev-ref HEAD 2>/dev/null"
+prompt_set_flag container solarized.yellow "$(container_type)"
+prompt_set_hook bzr solarized.orange "bzr_pipe_if_different"
+prompt_set_hook git solarized.orange "git rev-parse --abbrev-ref HEAD 2>/dev/null"
 prompt_set extended
