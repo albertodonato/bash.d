ea42f9b61ed25de65cf95425a2394fd0b976f2b6
Alberto Donato <alberto.donato@gmail.com>
2017-04-06 01:15:05 +0200

    Add completion for s().

--- lib/misc.sh
+++ lib/misc.sh
@@ -1,6 +1,6 @@
 # -*- mode: sh -*-
 #
-# Miscellaneous functions.
+# Miscellaneous shell-related functions.
 #
 
 #
@@ -12,6 +12,17 @@ bool() {
     "$@" && echo "true" || echo "false"
 }
 
+
+#
+# Return whether the shell is interactive.
+#
+# Usage: is_interactive
+#
+is_interactive() {
+    echo $- | grep -q i
+}
+
+
 #
 # Source a system file.
 # If no filename is given, list the available names.
@@ -19,7 +30,7 @@ bool() {
 # Usage: s [<filename>, ...]
 #
 s() {
-    local sourcedir=$SYSTEM_DIR/source
+    local sourcedir="$SYSTEM_DIR/source"
     if [ $# -eq 0 ]; then
         ls -1 $sourcedir;
         return
@@ -30,3 +41,16 @@ s() {
         . "$sourcedir/$file"
     done
 }
+
+#
+# Autocomplete helper for the s() command.
+#
+if is_interactive; then
+    _s() {
+        local cur=${COMP_WORDS[COMP_CWORD]}
+        local sources="$(ls -1 $SYSTEM_DIR/source)"
+        COMPREPLY=( $(compgen -W "$sources" -- "$cur") )
+    }
+
+    complete -F _s s
+fi
