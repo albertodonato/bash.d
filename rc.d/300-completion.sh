c72e0df2bbde71c1ff0ab51c7968b5ae032990b0
Alberto Donato <alberto.donato@gmail.com>
2017-05-02 10:06:33 +0200

    Add completion for ssh-lxd.

--- rc.d/300-completion.sh
+++ rc.d/300-completion.sh
@@ -19,4 +19,11 @@ if is_interactive; then
         COMPREPLY=( $(compgen -W "$sources" -- "$cur") )
     }
     complete -F _ve ve
+
+    _ssh_lxd() {
+        local cur=${COMP_WORDS[COMP_CWORD]}
+        local sources="$(lxc list --columns=ns | awk '$4 == "RUNNING" { print $2; }')"
+        COMPREPLY=( $(compgen -W "$sources" -- "$cur") )
+    }
+    complete -F _ssh_lxd ssh-lxd
 fi
