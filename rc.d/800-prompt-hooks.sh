676681396490395bc37e99eb5898cd72cb69c8a5
Alberto Donato <alberto.donato@gmail.com>
2017-10-04 14:12:26 +0200

    change color
    Signed-off-by: Alberto Donato <alberto.donato@gmail.com>

--- rc.d/800-prompt-hooks.sh
+++ rc.d/800-prompt-hooks.sh
@@ -6,5 +6,5 @@
 prompt_set_flag container solarized.yellow "$(container_type)"
 prompt_set_hook bzr solarized.orange "bzr_pipe_if_different"
 prompt_set_hook git solarized.orange "git rev-parse --abbrev-ref HEAD 2>/dev/null"
-prompt_set_hook go solarized.green '[ "$GOPATH" != "$HOME/go" ] && echo $GOPATH'
+prompt_set_hook go solarized.yellow '[ "$GOPATH" != "$HOME/go" ] && echo $GOPATH'
 prompt_set extended
