5029dd08e73afe7a38c7f551ef9baf62f3fb26b9
Alberto Donato <alberto.donato@gmail.com>
2020-03-11 17:49:39 +0100

    include git hash in prompt

---
+++ lib/devel-git.sh
@@ -0,0 +1,15 @@
+# -*- mode: sh -*-
+# shellcheck disable=SC2039
+#
+# Utility functions for Git.
+
+
+# Return a git reference (if available for the prompt)
+git_prompt_ref() {
+    local ref hash
+    hash=$(git rev-parse --short HEAD 2>/dev/null)
+    ref=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+    if [ -n "$hash" ] && [ -n "$ref" ]; then
+        echo "$ref:$hash"
+    fi
+}
--- rc.d/800-prompt-hooks.sh
+++ rc.d/800-prompt-hooks.sh
@@ -4,7 +4,8 @@
 
 
 prompt_set_flag container solarized.yellow "$(container_type)"
-prompt_set_hook git solarized.orange "git rev-parse --abbrev-ref HEAD 2>/dev/null"
+# shellcheck disable=SC2016
+prompt_set_hook git solarized.orange 'echo $(git_prompt_ref)'
 # shellcheck disable=SC2016
 prompt_set_hook go solarized.yellow '[ "$GOPATH" != "$HOME/go" ] && echo $GOPATH'
 # shellcheck disable=SC2016
