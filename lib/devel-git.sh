bb8d00527d181f9811082aedd1ab072ccdb3e92d
Alberto Donato <alberto.donato@gmail.com>
2020-03-12 19:33:23 +0100

    better separator for git prompt

--- lib/devel-git.sh
+++ lib/devel-git.sh
@@ -10,6 +10,6 @@ git_prompt_ref() {
     hash=$(git rev-parse --short HEAD 2>/dev/null)
     ref=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
     if [ -n "$hash" ] && [ -n "$ref" ]; then
-        echo "[$hash]$ref"
+        echo "$hash|$ref"
     fi
 }
