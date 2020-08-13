a190f02a9ab812e283a176f6cc588db7b11aa95f
Alberto Donato <alberto.donato@gmail.com>
2020-08-13 17:37:52 +0200

    add neofetch alias

--- rc.d/200-alias.sh
+++ rc.d/200-alias.sh
@@ -22,10 +22,11 @@ alias ec="emacs -batch -f batch-byte-compile"
 
 alias ipy="ipython3"
 alias pytree='tree -P \*\.py  -I test_\*\.py -I \*\.pyc -I \*\.pyo -I \*\.egg-info -I __pycache__'
+alias pytrove="curl -s 'https://pypi.org/pypi?:action=list_classifiers'"
 
 # shellcheck disable=SC2142
 alias diffcount="diffstat -s | awk -F, '{ gsub(/[^0-9,]/, \"\"); print \$2+\$3; }'"
 
 alias jpgpdf="convert -compress JPEG -resize 1240x1753 -page A4 -units PixelsPerInch -density 150x150"
 alias publicip="curl -s  http://checkip.dyn.com/ | sed 's/.* \\([0-9.]\\+\\)<.*/\\1/'"
-alias pytrove="curl -s 'https://pypi.org/pypi?:action=list_classifiers'"
+alias neofetch="curl -s https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch | bash"
