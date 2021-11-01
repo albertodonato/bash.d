3643cbbf20b6db12c5da6a430a9f0e359719a96a
Alberto Donato <alberto.donato@gmail.com>
2021-11-01 10:14:25 +0100

    add cargo path

--- rc.d/000-vars.sh
+++ rc.d/000-vars.sh
@@ -7,6 +7,7 @@
 [ -z "$_PATH" ] && _PATH="$PATH"
 PATH="$HOME/bin:$SYSTEM_DIR/bin:$SYSTEM_BASH_DIR/bin:$_PATH"
 PATH+=":$HOME/.local/bin"
+PATH+=":$HOME/.cargo/bin"
 
 export GOPATH="$HOME/go"
 PATH+=":$GOPATH/bin"
