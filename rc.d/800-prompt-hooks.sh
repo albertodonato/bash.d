7aa423ee8d3d074c4bd1a7608b46e9313e841811
Alberto Donato <alberto.donato@gmail.com>
2023-04-05 16:05:13 +0200

    update git_prompt_ref, show hash first

--- rc.d/800-prompt-hooks.sh
+++ rc.d/800-prompt-hooks.sh
@@ -3,7 +3,7 @@
 
 prompt_set_flag container solarized.yellow "$(container_type)"
 # shellcheck disable=SC2016
-prompt_set_hook git solarized.orange 'echo $(git_prompt_ref)'
+prompt_set_hook git solarized.orange 'git_prompt_ref'
 # shellcheck disable=SC2016
 prompt_set_hook go solarized.yellow '[ "$GOPATH" != "$HOME/go" ] && echo $GOPATH'
 # shellcheck disable=SC2016
