80a291f3198a6c1b9daf93552f041f9624b5e8ae
Alberto Donato <alberto.donato@gmail.com>
2023-06-16 09:37:47 +0200

    drop old pytree

--- rc.d/200-alias.sh
+++ rc.d/200-alias.sh
@@ -10,18 +10,17 @@ r() {
     rg -p "$@" | less -R
 }
 
-pytree() {
-    find "$1" -not -regex '.*__pycache__.*' | tree --fromfile
-}
-
 alias e="emacsclient -n"
 alias ee="emacsclient -e"
 alias ec="emacs -batch -f batch-byte-compile"
 alias enw="emacsclient -q -nw"
 
 alias ipy="ipython3"
-alias pytree='tree -P \*\.py  -I test_\*\.py -I \*\.pyc -I \*\.pyo -I \*\.egg-info -I __pycache__'
 alias pytrove="curl -s 'https://pypi.org/pypi?:action=list_classifiers'"
+
+pytree() {
+    find "$1" -not -regex '.*__pycache__.*' | tree --fromfile
+}
 
 # shellcheck disable=SC2142
 alias diffcount="diffstat -s | awk -F, '{ gsub(/[^0-9,]/, \"\"); print \$2+\$3; }'"
