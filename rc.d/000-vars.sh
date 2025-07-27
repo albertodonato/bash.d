ab96fd228cfccdfc863d99e2315fd428aa74ebbc
Alberto Donato <alberto.donato@gmail.com>
2025-07-27 20:02:14 +0200

    remove path vars

--- rc.d/000-vars.sh
+++ rc.d/000-vars.sh
@@ -3,15 +3,7 @@
 
 # Keep the original path, so it can be reapplied on reload
 [ -z "$_PATH" ] && _PATH="$PATH"
-PATH="$HOME/bin:$SYSTEM_DIR/bin:$SYSTEM_BASH_DIR/bin:$_PATH"
-PATH+=":$HOME/.local/bin"
-PATH+=":$HOME/.cargo/bin"
-PATH+=":$HOME/npm/bin"
-PATH+=":$HOME/venv/bin"
-
-export GOPATH="$HOME/go"
-PATH+=":$GOPATH/bin"
+PATH="$SYSTEM_DIR/bin:$SYSTEM_BASH_DIR/bin:$_PATH"
 
 export HISTFILESIZE=100000
-
 export LESS="-R"
