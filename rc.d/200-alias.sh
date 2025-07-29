16d66c143b2fed22e75f7232e5e6d64343f098dd
Alberto Donato <alberto.donato@gmail.com>
2025-07-29 13:54:02 +0200

    chore: update/cleanup aliases

--- rc.d/200-alias.sh
+++ rc.d/200-alias.sh
@@ -16,7 +16,6 @@ alias ec="emacs -batch -f batch-byte-compile"
 alias enw="emacsclient -q -nw"
 
 alias ipy="ipython3"
-alias pytrove="curl -s 'https://pypi.org/pypi?:action=list_classifiers'"
 
 pytree() {
     find "$1" -not -regex '.*__pycache__.*' | tree --fromfile
@@ -24,11 +23,6 @@ pytree() {
 
 alias dds="dd status=progress oflag=sync"
 
-# shellcheck disable=SC2142
-alias diffcount="diffstat -s | awk -F, '{ gsub(/[^0-9,]/, \"\"); print \$2+\$3; }'"
-
 alias noansi="sed -u 's/\x1b\[[0-9;]*m//g'"
 
 alias jpgpdf="convert -compress JPEG -resize 1240x1753 -page A4 -units PixelsPerInch -density 150x150"
-alias publicip="curl -s http://whatismyip.akamai.com/ && echo"
-alias neofetch="curl -s https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch | bash"
