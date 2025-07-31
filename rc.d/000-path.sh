21f99ce1812a7e2904e052711f824b2229be8b8d
Alberto Donato <alberto.donato@gmail.com>
2025-07-31 21:43:02 +0200

    chore: cleanups

--- rc.d/000-path.sh
+++ rc.d/000-path.sh
@@ -4,4 +4,3 @@
 # Keep the original path, so it can be reapplied on reload
 [ -z "$_PATH" ] && _PATH="$PATH"
 PATH="$SHELL_D/bin:$_PATH"
-
--- rc.d/800-prompt-hooks.bash
+++ rc.d/800-prompt-hooks.bash
@@ -6,7 +6,6 @@ prompt_set_hook git yellow 'git_prompt_ref'
 # shellcheck disable=SC2016
 prompt_set_hook go yellow '[ "$GOPATH" != "$HOME/go" ] && echo $GOPATH'
 # shellcheck disable=SC2016
-prompt_set_hook ve red '[ -n "$VIRTUAL_ENV" ] && echo $(basename $VIRTUAL_ENV)'
+prompt_set_hook ve red '[ "$VIRTUAL_ENV" ] && echo $(basename $VIRTUAL_ENV)'
 # shellcheck disable=SC2016
-prompt_set_hook nix-shell lightred '[ -n "$IN_NIX_SHELL" ] && echo nis-shell'
 prompt_set extended
