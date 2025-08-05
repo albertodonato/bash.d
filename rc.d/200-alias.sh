9e61da551c426bb9d15c6d9e770b9aa93033d673
Alberto Donato <alberto.donato@gmail.com>
2025-08-05 12:53:31 +0200

    chore: move python shorthands to aliases

--- rc.d/200-alias.sh
+++ rc.d/200-alias.sh
@@ -21,8 +21,17 @@ pytree() {
     find "$1" -not -regex '.*__pycache__.*' | tree --fromfile
 }
 
+delpyc() {
+    find "$@" -type d -name __pycache__ -print0 | xargs --null rm -rf
+    find "$@" -name \*.pyc -exec rm {} \;
+}
+
 alias dds="dd status=progress oflag=sync"
 
 alias noansi="sed -u 's/\x1b\[[0-9;]*m//g'"
 
 alias jpgpdf="convert -compress JPEG -resize 1240x1753 -page A4 -units PixelsPerInch -density 150x150"
+
+deltilde() {
+    find "$@" -name \*~ -exec rm {} \;
+}
